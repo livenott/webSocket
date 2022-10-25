5031cb6fe90314c653e8218aedffa3cb444c90c2
Elizaveta Egorova <elizavetaegorova@MacBook-Pro-Elizaveta-2.local>
2022-10-26 00:34:58 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles/index.scss";
+
+console.log('Hello World');
+
+var animalObjects = {
+  cow: 3,
+  hourse: 2,
+};
+
+var animalDiv = document.body.appendChild(document.createElement('div'))
+
+for (const prop in animalObjects) {
+    animalDiv.textContent += `animalObjects.${prop} = ${animalObjects[prop]}\n`;
+  }
